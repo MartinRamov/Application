f2d0b0fe103ba72d0ece243b41c9ff39ed7e3208
MilaG145 <milagurova@yahoo.com>
2017-06-28 18:42:33 +0200

    Test Commit

--- src/main/java/com/example/mm/model/User.java
+++ src/main/java/com/example/mm/model/User.java
@@ -1,4 +1,4 @@
-package com.example.mm.Model;
+package com.example.mm.model;
 
 import javax.persistence.Entity;
 
@@ -6,6 +6,6 @@ import javax.persistence.Entity;
  * Created by Win8.1 on 28.06.2017.
  */
 @Entity
-public class Activity {
+public class User {
 
 }
